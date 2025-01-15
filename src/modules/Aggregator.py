aaa2e5ce26998cc86b2626525760094c783d5f82
Andrew0328 <577971450@qq.com>
2025-01-15 23:47:25 +0800

    update Aggregator

--- src/modules/Aggregator.py
+++ src/modules/Aggregator.py
@@ -1,6 +1,9 @@
 import torch.nn as nn
 from modules.Hyperbolic import *
 from torch_scatter import scatter_mean
+from torch_scatter import scatter_softmax
+from torch_scatter import scatter_sum
+import torch.nn.functional as F
 
 
 class Aggregator(nn.Module):
@@ -11,43 +14,23 @@ class Aggregator(nn.Module):
     def __init__(self):
         super(Aggregator, self).__init__()
 
-    def forward(self, ego_embed, edge_index, edge_type, relation_weight):
+    def forward(self, ego_embed, edge_index, edge_type, relation_weights):
         head, tail = edge_index
         relation_type = edge_type
         n_entities = ego_embed.shape[0]
 
         head_emb = ego_embed[head]
         tail_emb = ego_embed[tail]
-        relation_emb = relation_embed[relation_type]
 
-        # hyperbolic
-        # hyper_head_emb = expmap0(head_emb)
-        # hyper_tail_emb = expmap(tail_emb, hyper_head_emb)
-        # hyper_relation_emb = expmap(relation_emb, hyper_head_emb)
-        # res = project(mobius_add(hyper_tail_emb, hyper_relation_emb))
-        # res = logmap(res, hyper_head_emb)
-        # entity_agg = scatter_mean(src=res, index=head, dim_size=n_entities, dim=0)
+        W_r = relation_weights[relation_type]
+        h_T = head_emb.unsqueeze(1)
+        transformed_head = torch.bmm(h_T, W_r).squeeze(1)
+        attention_scores = F.leaky_relu((transformed_head * tail_emb).sum(dim=1))
+        attention_scores_tail = torch.exp(attention_scores)
+        attention_scores_sum = scatter_sum(src=attention_scores_tail, index=head, dim_size=n_entities, dim=0)[head]
+        norm_attention_scores = attention_scores_tail / attention_scores_sum
+        message = tail_emb
+        weighted_messages = message * norm_attention_scores.unsqueeze(-1)  # [num_edges, embed_dim]
+        aggregated_messages = scatter_mean(src=weighted_messages, index=head, dim=0)  # [num_nodes, embed_dim]
 
-        # neigh_relation_emb = tail_emb * relation_emb
-        # neigh_relation_emb_weight = self.calculate_sim_hrt(head_emb, tail_emb, relation_emb)
-        # neigh_relation_emb_weight = neigh_relation_emb_weight.expand(neigh_relation_emb.shape[0],
-        #                                                              neigh_relation_emb.shape[1])
-        # # neigh_relation_emb_tmp = torch.matmul(neigh_relation_emb_weight, neigh_relation_emb)
-        # neigh_relation_emb_weight = scatter_softmax(neigh_relation_emb_weight, index=head, dim=0)
-        # neigh_relation_emb = torch.mul(neigh_relation_emb_weight, neigh_relation_emb)
-        # entity_agg = scatter_sum(src=neigh_relation_emb, index=head, dim_size=n_entities, dim=0)
-
-        # # euclidean
-        # res1 = tail_emb + relation_emb
-        # entity_agg = scatter_mean(src=res1, index=head, dim_size=n_entities, dim=0)
-
-        return entity_agg
-
-    # def calculate_sim_hrt(self, entity_emb_head, entity_emb_tail, relation_emb):
-    #     tail_relation_emb = entity_emb_tail * relation_emb
-    #     tail_relation_emb = tail_relation_emb.norm(dim=1, p=2, keepdim=True)
-    #     head_relation_emb = entity_emb_head * relation_emb
-    #     head_relation_emb = head_relation_emb.norm(dim=1, p=2, keepdim=True)
-    #     att_weights = torch.matmul(head_relation_emb.unsqueeze(dim=1), tail_relation_emb.unsqueeze(dim=2)).squeeze(dim=-1)
-    #     att_weights = att_weights ** 2
-    #     return att_weights
+        return aggregated_messages
